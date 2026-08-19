a95690a2723812e0f96a6f95606875c3fbd3de7e
chhaylymeng9-dotcom <chhaylymeng9@gmail.com>
2026-08-20 04:09:36 +0700

    Initial commit

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  Base: process.env.VITE_BASE_PATH || "/JongCham",
-});
